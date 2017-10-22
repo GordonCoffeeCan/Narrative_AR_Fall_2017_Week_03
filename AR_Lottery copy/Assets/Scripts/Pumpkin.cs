6618c8964e1a30ef3d3ee839afbe85f05b37f1c5
Woraya : Mint <wb743@nyu.edu>
2017-10-22 02:04:13 -0400

    ui text update and sound

---
+++ AR_Lottery copy/Assets/Scripts/Pumpkin.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pumpkin : MonoBehaviour {
+
+    [HideInInspector] public bool simulatePhysics = true;
+    [HideInInspector] public bool selfDestroy = true;
+
+    private float destroyTimer = 3;
+    private float gravity = 9.8f;
+    private Rigidbody rig;
+
+    //private float currentGravitySpeed = 0;
+
+	// Use this for initialization
+	void Start () {
+        rig = this.GetComponent<Rigidbody>();
+
+        simulatePhysics = true;
+        selfDestroy = true;
+}
+	
+	// Update is called once per frame
+	void Update () {
+        if (selfDestroy) {
+            destroyTimer -= Time.deltaTime;
+
+            if (destroyTimer <= 0) {
+                Destroy(this.gameObject);
+            }
+        }
+
+        Debug.Log(simulatePhysics);
+
+        if (simulatePhysics == false) {
+            rig.isKinematic = true;
+        }
+    }
+
+    private void FixedUpdate() {
+        if (simulatePhysics) {
+            rig.AddForce(Vector3.back * gravity, ForceMode.Acceleration);
+        }
+    }
+}
---
+++ AR_Lottery copy/Assets/Scripts/TouchModel.cs
@@ -0,0 +1,25 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class TouchModel : MonoBehaviour {
+
+//	// Use this for initialization
+//	void Start () {
+		
+//	}
+	
+//	// Update is called once per frame
+//	void Update () {
+//        if (Input.touchCount > 0 && Input.GetTouch((0).phase == TouchPhase.Began)
+//        {
+//            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+//            RaycastHit hit;
+
+//            if (Physics.Raycast(ray, out hit) && hit.transform.gameObject.name == "myGameObjectName")
+//            {
+//                hit.GetComponent<TouchObjectScript>().ApplyForce();
+//            }
+//        }
+//	}
+//}
